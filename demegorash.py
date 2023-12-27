06d82a292e517cec5bc66a707b106c90fc05d157
Demegorash <felipe.solisgranados@gmail.com>
2023-12-26 20:27:46 -0600

    Este es mi primer commit del roadmap 2024 de Mouredev

---
+++ demegorash.py
@@ -0,0 +1,11 @@
+# Hello, this is a comment showing the official website for Python: https://www.python.org
+
+# This is a single line comment.
+
+"""
+Here, I can show and type
+a multi-line code.  As you can
+see, I am using double coutes in order
+to show it.  Nice right?
+"""
+
