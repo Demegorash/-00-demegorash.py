4dc523b20218d1593eead3ea79155c373855ab33
Demegorash <felipe.solisgranados@gmail.com>
2023-12-26 21:58:10 -0600

    Este es el trabajo para entregar a Mouredev

--- demegorash.py
+++ demegorash.py
@@ -9,3 +9,37 @@ see, I am using double coutes in order
 to show it.  Nice right?
 """
 
+# This is going to be a varible example
+
+name = "Demegorash"
+age = 45
+country = "Costa Rica"
+
+print("My name is:", name, ".", "I am ", age, "years old", ".", "I live in: ", country, ".")
+
+# This is going to be a constant example
+
+PI = 3.1415927
+C =  299792458
+
+print(PI)
+print(C)
+print(PI, "is Pi and", C, "is ligth speed in the vacuum (we represent it as c).")
+
+
+# Some examples of data in python
+
+string_example = "This is a string example"
+print(type(string_example))
+integer = 42
+print(type(integer))
+float = 3.14
+print(type(float))
+boolean = True
+print(type(boolean))
+complex_number = 2 + 3j
+print(type(complex_number))
+
+# This is an example of a function named print
+this_example = "Python"
+print("Hola, ", this_example, "!!!")
